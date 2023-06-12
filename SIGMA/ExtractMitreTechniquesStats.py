5b22ff5e2404646036b0f28571aea8edbf09b423
Bert-Janp <bertjanpals@gmail.com>
2023-06-12 15:27:52 +0200

    Update ExtractMitreTechniquesStats.py

--- SIGMA/ExtractMitreTechniquesStats.py
+++ SIGMA/ExtractMitreTechniquesStats.py
@@ -1,5 +1,5 @@
 # This script can be put in a directory with SIGMA rules. The script will output statistics on the mitre techniques and tactics that are present in an output file named: mitre_statistics.txt
-
+# List will be done both on current directory as well as child directories.
 import os
 import yaml
 from collections import Counter
@@ -7,17 +7,18 @@ from collections import Counter
 def count_tag_values_in_yaml_files(directory, key_tag):
     tag_values = []
 
-    for filename in os.listdir(directory):
-        if filename.endswith('.yml'):
-            file_path = os.path.join(directory, filename)
-            with open(file_path, 'r') as file:
-                try:
-                    data = yaml.safe_load(file)
-                    if key_tag in data:
-                        value = data[key_tag]
-                        tag_values.extend(value)
-                except yaml.YAMLError as e:
-                    print(f"Error reading {filename}: {e}")
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if filename.endswith('.yml'):
+                file_path = os.path.join(root, filename)
+                with open(file_path, 'r') as file:
+                    try:
+                        data = yaml.safe_load(file)
+                        if key_tag in data:
+                            value = data[key_tag]
+                            tag_values.extend(value)
+                    except yaml.YAMLError as e:
+                        print(f"Error reading {filename}: {e}")
 
     tag_counts = Counter(tag_values)
     return tag_counts
